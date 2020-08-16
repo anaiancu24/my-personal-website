2286f3b3a66248f2b9157d66b6db7d4e3da05e84
anaiancu24 <ana.iulia24@icloud.com>
2020-08-16 12:58:40 +0200

    Small copy changes

--- components/About.vue
+++ components/About.vue
@@ -2,8 +2,18 @@
 <div class="section_50-50 section-85 center">
 <div class="section_50-50_left">
     <h3>Some facts about me</h3>
-    <p>I'm a Full Stack Developer from Romania, living in Amsterdam. I'm on a mission to discover as much as possible about web. From design to development, analytics, data, machine learning, I want to know it all. </p> 
-    <p>I don't have any particular talents, but I do have the ability and the willing to learn new skills all the time.</p> 
+    <p>I'm a Full Stack Developer and a Digital Marketing
+enthusiast. I'm on a mission to discover as much as possible about web. From design to development, analytics, data, machine learning, I want to know it all. </p> 
+    <p>With a background in marketing, I gained valuable
+skills such as data analysis, SEO, UI/UX, branding,
+growth hacking, social media and many more from 
+working in different projects. </p> 
+<p>I am skilled at writing well-designed, testable and 
+efficient code using current best practices in 
+Web development. </p>
+<p>Having a background in marketing and experience in 
+web dev, I want to bring these two worlds together 
+by sharing my knowledge with anyone interested.</p>
     <p>I'm open for new projects so if you think you have something challenging, <a href="/contact"><span style="color:#00ff7b">let's have a chat</span></a>!</p> 
 </div>
 <div class="section_50-50_right">
